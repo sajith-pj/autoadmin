b02e688bcb557ba143f2ca761b75cee54102f528
arjun <arjunarunachalan@gmail.com>
2024-01-26 23:42:54 +0530

    pages dashboard created

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ import ClientPage from "./Pages/ClientPanel/ClientPage";
 import Register from "./Pages/Register/Register";
 import Login from "./Pages/Login";
 import { GoogleOAuthProvider } from "@react-oauth/google";
+import "./assets/css/table.scss"
 
 config(axiosConfig);
 function App() {
--- src/Pages/Pages/index.jsx
+++ src/Pages/Pages/index.jsx
@@ -1,13 +1,40 @@
-import Form from "../../components/Form/Form";
+import Table from "../../components/Table";
 import { creatPageForm } from "../../utils/pageforms/forms";
+import DashboardImage from "../../assets/images/Mask group.png";
+
 const Page = () => {
+    const tableHeader = [
+      { header: "No" },
+      {
+        header: "image",
+        render: () => (
+          <img src={DashboardImage} alt="Dashboard" width={30} height={30} />
+        ),
+      },
+      { header: "Name" },
+      { header: "path" },
+    ];
+    const tableRows = [
+      {
+        No: "1",
+        render: () => <img src={DashboardImage} alt="Dashboard" width={30} height={30}/>,
+        Name: "Dashboard",
+        path: "/",
+      },
+      {
+        No: "1",
+        render: () => <img src={DashboardImage} alt="Dashboard Image" />,
+        Name: "Dashboard",
+        path: "/",
+      },
+    ];
   return (
     <div>
       <h1 className="text-primary text-xl font-semibold font-oswald">
         Create Your Page
       </h1>
       <div className="mt-4">
-        <Form {...creatPageForm} />
+        <Table header={tableHeader} rows={tableRows} />
       </div>
     </div>
   );
